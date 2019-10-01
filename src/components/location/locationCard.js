d61335f5fdffa7a9301e7fe63e5ec5718f04a0ae
unknown <mariobud@gmail.com>
2019-10-01 14:47:22 -0500

    master commit fix

--- src/components/location/locationCard.js
+++ src/components/location/locationCard.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import {firstLetterCase} from '../../modules/Helpers'
 
 class LocationCard extends Component {
     render() {
         return (
             <div className="card">
                 <div className="card-content">
-                    <p>Location: {this.props.object.locationName}</p>
+                    <p>Location: {firstLetterCase(this.props.object.locationName)}</p>
                     <p>Phone Number: {this.props.object.phone}</p>
                     <Link to={`/locations/${this.props.object.id}`}><button>Details</button></Link>
                     <button type="button" onClick={() => this.props.deleteLocation(this.props.object.id)}>Remove</button>
